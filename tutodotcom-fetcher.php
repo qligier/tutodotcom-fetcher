bdcbe4e7e1aea0e50c7191085a45a38baa37d2bf
Quentin Ligier <quentin.ligier@bluewin.ch>
2015-09-05 21:48:06 +0200

    Arguments now parsed by CLImate

--- tutodotcom-fetcher.php
+++ tutodotcom-fetcher.php
@@ -2,34 +2,88 @@
 <?php
 require_once 'vendor/autoload.php';
 $cli = new League\CLImate\CLImate;
+$cli->description('tutodotcom-fetcher is a small program that downloads tuto.com tutorial videos');
+$cli->arguments->add([
+    'id' => [
+        'prefix' => 'i',
+        'longPrefix' => 'id',
+        'description' => 'The ID(s) of the tuto to download (comma-separated)',
+        'required'    => true,
+    ],
+    'user' => [
+        'prefix'      => 'u',
+        'longPrefix'  => 'user',
+        'description' => 'The email address to log in with',
+        'required'    => true,
+    ],
+    'password' => [
+        'prefix'      => 'p',
+        'longPrefix'  => 'password',
+        'description' => 'The password to log in with',
+        'required'    => true,
+    ],
+    'output' => [
+        'prefix'      => 'o',
+        'longPrefix'  => 'output',
+        'description' => 'The output directory',
+        'required'    => true,
+    ],
+    'help' => [
+        'prefix'      => 'h',
+        'longPrefix'  => 'help',
+        'description' => 'Prints an usage statement',
+        'noValue'     => true,
+    ],
+]);
 
-define('PADDING', 75);
+define('PADDING', 60);
 $browser_headers = [
     'User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0',
-    'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', // */
     'Accept-Language: fr,fr-FR;q=0.8,en-US;q=0.5,en;q=0.3',
 ];
 
 
 
+
+
+
+
 // Check: arguments
-$opts = getopt('i:u:p:o:h');
-if (!empty($opts['h']) || empty($opts['i']) || empty($opts['u']) || empty($opts['p']) || empty($opts['o'])) {
-    $cli->out('Usage: php tutodotcom-fetcher.php -i [id] -u [user] -p [password] -o [output dir]');
-    $cli->out(' -i       : The ID(s) of the tuto to download (comma-separated)');
-    $cli->out(' -u       : The email to log in with');
-    $cli->out(' -p       : The password to log in with');
-    $cli->out(' -o       : The output directory');
-    $cli->out('By example:');
-    $cli->blue()->out('php tutodotcom-fetcher.php -i 49913,49140 -u email@domain.com -p password -o /home/tuto.com/videos/');
+$arguments_valid = true;
+try {
+    $cli->arguments->parse();
+}
+catch (Exception $e) {
+    $arguments_valid = false;
+    if (!$cli->arguments->defined('help'))
+        $cli->red()->error($e->getMessage());
+}
+
+if (!$arguments_valid || $cli->arguments->defined('help')) {
+    $cli->usage();
     die;
 }
 
 
+
+// Check: output directory
+$cli->inline(str_pad('Output directory exists and is writable', PADDING));
+if (!is_dir($cli->arguments->get('output')) || !is_writeable($cli->arguments->get('output'))) {
+    $cli->inline('[')->red()->inline('FAIL')->white()->out(']');
+    $cli->red()->out('Please choose a writable directory!');
+    die;
+}
+$cli->inline('[ ')->green()->inline('OK')->white()->out(' ]');
+define('OUTPUT_DIR', realpath($cli->arguments->get('output')).'/');
+
+
+
+
 // Check: input
 $input_ids = [];
-if (!empty($opts['i'])) {
-    $input_ids = explode(',', $opts['i']);
+if (!empty($cli->arguments->get('id'))) {
+    $input_ids = explode(',', $cli->arguments->get('id'));
     array_walk($input_ids, 'trim');
     $input_ids = array_filter($input_ids, 'is_numeric');
     $cli->inline(str_pad('Input: '.count($input_ids).' links', PADDING));
@@ -39,18 +93,6 @@ if (!empty($opts['i'])) {
     }
     $cli->inline('[ ')->green()->inline('OK')->white()->out(' ]');
 }
-
-
-
-// Check: output directory
-$cli->inline(str_pad('Output directory exists and is writable', PADDING));
-if (!is_dir($opts['o']) || !is_writeable($opts['o'])) {
-    $cli->inline('[')->red()->inline('FAIL')->white()->out(']');
-    $cli->red()->out('Please choose a writable directory!');
-    clean_and_die();
-}
-$cli->inline('[ ')->green()->inline('OK')->white()->out(' ]');
-$opts['o'] = realpath($opts['o']).'/';
 
 
 
@@ -71,7 +113,7 @@ $curl = curl_init();
 curl_setopt($curl, CURLOPT_URL, 'https://fr.tuto.com/connexion/');
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_COOKIESESSION, true);
-curl_setopt($curl, CURLOPT_COOKIEJAR, $opts['o'].'tmp_cookies.txt');
+curl_setopt($curl, CURLOPT_COOKIEJAR, OUTPUT_DIR.'tmp_cookies.txt');
 curl_setopt($curl, CURLOPT_HTTPHEADER, $browser_headers);
 $login_page = curl_exec($curl);
 curl_close($curl);
@@ -91,13 +133,13 @@ $curl = curl_init();
 curl_setopt($curl, CURLOPT_URL, 'https://fr.tuto.com/connexion/?redirect=http://fr.tuto.com/');
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_COOKIESESSION, true);
-curl_setopt($curl, CURLOPT_COOKIEJAR, $opts['o'].'tmp_cookies.txt');
-curl_setopt($curl, CURLOPT_COOKIEFILE, $opts['o'].'tmp_cookies.txt');
+curl_setopt($curl, CURLOPT_COOKIEJAR, OUTPUT_DIR.'tmp_cookies.txt');
+curl_setopt($curl, CURLOPT_COOKIEFILE, OUTPUT_DIR.'tmp_cookies.txt');
 curl_setopt($curl, CURLOPT_POST, true);
 curl_setopt($curl, CURLOPT_POSTFIELDS, [
     'tuto-csrf' => $csrf_token,
-    'login'     => urlencode($opts['u']),
-    'pwd'       => urlencode($opts['p']),
+    'login'     => urlencode($cli->arguments->get('user')),
+    'pwd'       => urlencode($cli->arguments->get('password')),
     'submit_login' => 'se+connecter',
 ]);
 curl_setopt($curl, CURLOPT_HTTPHEADER, $browser_headers);
@@ -118,13 +160,14 @@ $cli->inline('[ ')->green()->inline('OK')->white()->out(' ]');
 
 // Tutorials
 $video_links = [];
+$cli->blue()->out('Downloading '.count($input_ids).' tutorials');
 foreach ($input_ids AS $input_id) {
     $curl = curl_init();
     curl_setopt($curl, CURLOPT_URL, 'http://fr.tuto.com/compte/achats/video/'.$input_id.'/player/');
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($curl, CURLOPT_COOKIESESSION, true);
-    curl_setopt($curl, CURLOPT_COOKIEJAR, $opts['o'].'tmp_cookies.txt');
-    curl_setopt($curl, CURLOPT_COOKIEFILE, $opts['o'].'tmp_cookies.txt');
+    curl_setopt($curl, CURLOPT_COOKIEJAR, OUTPUT_DIR.'tmp_cookies.txt');
+    curl_setopt($curl, CURLOPT_COOKIEFILE, OUTPUT_DIR.'tmp_cookies.txt');
     curl_setopt($curl, CURLOPT_HTTPHEADER, $browser_headers);
     $tutorial_page = curl_exec($curl);
     curl_close($curl);
@@ -147,7 +190,7 @@ foreach ($input_ids AS $input_id) {
         $chapters_uid[$chapters_preg[1][$j]] = $chapters_preg[2][$j];
 
     if (1 === count($chapters_uid)) {
-        $cli->inline(str_pad('['.$input_id.'] Downloading '.$tutorial_title, PADDING));
+        $cli->inline(str_pad('['.$input_id.'] '.$tutorial_title, PADDING));
         $chapter_uid = array_keys($chapters_uid)[0];
         $chapter_title = (!empty(array_values($chapters_uid)[0])) ? array_values($chapters_uid)[0] : $tutorial_title_sanitized;
 
@@ -156,16 +199,16 @@ foreach ($input_ids AS $input_id) {
             continue 1;
         }
 
-        $result_hls = exec('./hls-fetch -f --playlist '.escapeshellarg($tutorial_playlist->{$chapter_uid}->urls->hls).' --output='.escapeshellarg($opts['o'].$chapter_title.'.ts'));
+        $result_hls = exec('./hls-fetch -f --playlist '.escapeshellarg($tutorial_playlist->{$chapter_uid}->urls->hls).' --output='.escapeshellarg(OUTPUT_DIR.$chapter_title.'.ts'));
         if (preg_match('/\d+\/\d+/', $result_hls))
             $cli->inline('[ ')->green()->inline('OK')->white()->out(' ]');
         else
             $cli->inline('[')->red()->inline('FAIL')->white()->out(']');
     }
     else {
-        $cli->out('['.$input_id.'] Downloading '.$tutorial_title);
+        $cli->out('['.$input_id.'] '.$tutorial_title.' ('.count($chapters_uid).' chapters)');
 
-        is_dir($opts['o'].$tutorial_title_sanitized) || mkdir($opts['o'].$tutorial_title_sanitized);
+        is_dir(OUTPUT_DIR.$tutorial_title_sanitized) || mkdir(OUTPUT_DIR.$tutorial_title_sanitized);
         $j = 0;
         foreach ($chapters_uid AS $chapter_uid => $chapter_title) {
             ++$j;
@@ -176,7 +219,7 @@ foreach ($input_ids AS $input_id) {
                 continue 1;
             }
 
-            $result_hls = exec('./hls-fetch -f --playlist '.escapeshellarg($tutorial_playlist->{$chapter_uid}->urls->hls).' --output='.escapeshellarg($opts['o'].$tutorial_title_sanitized.'/'.$j.'-'.$chapter_title.'.ts'));
+            $result_hls = exec('./hls-fetch -f --playlist '.escapeshellarg($tutorial_playlist->{$chapter_uid}->urls->hls).' --output='.escapeshellarg(OUTPUT_DIR.$tutorial_title_sanitized.'/'.$j.'-'.$chapter_title.'.ts'));
             if (preg_match('/\d+\/\d+/', $result_hls))
                 $cli->inline('[ ')->green()->inline('OK')->white()->out(' ]');
             else
@@ -190,8 +233,8 @@ foreach ($input_ids AS $input_id) {
 // Cleaning
 clean_and_die();
 function clean_and_die() {
-    global $opts, $cli;
-    !is_file($opts['o'].'tmp_cookies.txt') || unlink($opts['o'].'tmp_cookies.txt');
+    global $cli;
+    !is_file(OUTPUT_DIR.'tmp_cookies.txt') || unlink(OUTPUT_DIR.'tmp_cookies.txt');
     $cli->blue()->out('Cleaning and quitting');
     die;
 }
